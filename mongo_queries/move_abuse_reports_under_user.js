20a6635c613e01bb48ead8e52be17d0a3d54950a
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-10-31 10:16:59 +0100

    forgot last part of script

--- mongo_queries/move_abuse_reports_under_user.js
+++ mongo_queries/move_abuse_reports_under_user.js
@@ -11,6 +11,12 @@ db.abuse_reports.find().forEach(function(data) {
   else{
 
     print('blind');
+    helper = db.users.findOne({_id:data.helper_id});
+    db.users.update({
+      '_id':helper._id
+    },
+     { $push: { abuse_reports: data } }
+    );
   }
 });
 
