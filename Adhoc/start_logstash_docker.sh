53436fbe30fb6779df33c7a7a325c7a8bb2ade6f
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-11-28 17:13:34 +0100

    update

--- Adhoc/start_logstash_docker.sh
+++ Adhoc/start_logstash_docker.sh
@@ -1,4 +1,2 @@
-sudo docker run -d -e LOGSTASH_CONFIG_URL="https://gist.githubusercontent.com/khebbie/42d72d212cf3727a03a0/raw/aae3cae7d9093560205e353c2439934a59cfa223/logstash.conf"  -p 9292:9292 -p 9200:9200 -p 9300:9300  -p 3334:3334/udp -p 3333:3333/udp pblittle/docker-logstash
+sudo docker run -d -v /data:/data -v /data/logstash-opt:/opt -e LOGSTASH_CONFIG_URL="https://gist.githubusercontent.com/khebbie/42d72d212cf3727a03a0/raw/068f4816693f3b1171fd7c7c7d7eaadaca8801be/logstash.conf"  -p 9292:9292 -p 9200:9200 -p 9300:9300  -p 3334:3334/udp -p 3333:3333/udp pblittle/docker-logstash
 
-
-sudo docker run -d -e LOGSTASH_CONFIG_URL="https://gist.githubusercontent.com/khebbie/42d72d212cf3727a03a0/raw/0f1012903163bd4f8124b72760b8b0c2985b9f5e/logstash.conf"  -p 9292:9292 -p 9200:9200 -p 9300:9300  -p 3334:3334/udp -p 3333:3333/udp pblittle/docker-logstash
