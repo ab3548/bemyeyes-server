173c184865755a876b73200cfdb778cec3cc9670
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-12-05 13:11:34 +0100

    reindent

--- models/user.rb
+++ models/user.rb
@@ -68,7 +68,7 @@ class User
       "$or" => [
         {:go_to_sleep_in_seconds_since_midnight.lte => now_in_seconds_since_midnight},
         {:wake_up_in_seconds_since_midnight.gte => now_in_seconds_since_midnight}
-    ])
+      ])
   end
 
   def is_logged_in?()
@@ -76,8 +76,8 @@ class User
   end
 
   def reset_expiry_time
-   now = Time.now
-   self.expiry_time = Time.new(now.year, now.month, now.day, 0, 0, 0) - 1.days.to_i
+    now = Time.now
+    self.expiry_time = Time.new(now.year, now.month, now.day, 0, 0, 0) - 1.days.to_i
   end
 
   #should be called on each login
@@ -89,111 +89,111 @@ class User
   def self.authenticate_using_email(email, password)
     user = User.first(:email => { :$regex => /#{Regexp.escape(email)}/i })
 
-                                  if !user.nil?
-                                    return authenticate_password(user, password)
-                                  end
+    if !user.nil?
+      return authenticate_password(user, password)
+    end
 
-                                  return nil
-                                  end
+    return nil
+  end
 
-                                  def self.authenticate_using_user_id(email, user_id)
-                                    user = User.first(:user_id => user_id)
-                                    if !user.nil?
-                                      return user
-                                    end
+  def self.authenticate_using_user_id(email, user_id)
+    user = User.first(:user_id => user_id)
+    if !user.nil?
+      return user
+    end
 
-                                    return nil
-                                  end
+    return nil
+  end
 
-                                  def password=(pwd)
-                                    @password = pwd
-                                  end
+  def password=(pwd)
+    @password = pwd
+  end
 
-                                  def snooze
-                                    if self.available_from && Time.now.utc < self.available_from
-                                      { "period" => self.snooze_period,
-                                        "until" => self.available_from
-                                        }
-                                    else
-                                      nil
-                                    end
-                                  end
+  def snooze
+    if self.available_from && Time.now.utc < self.available_from
+      { "period" => self.snooze_period,
+        "until" => self.available_from
+      }
+    else
+      nil
+    end
+  end
 
-                                  def to_json()
-                                    return { "id" => self._id,
-                                             "user_id" => self.user_id,
-                                             "email" => self.email,
-                                             "first_name" => self.first_name,
-                                             "last_name" => self.last_name,
-                                             "role" => self.role,
-                                             "languages" => self.languages,
-                                             "snooze" => self.snooze,
-                                             "auth_token" => auth_token,
-                                             "expiry_time" => expiry_time,
-                                             "facebook_user" => is_external_user
-                                             }.to_json
-                                  end
+  def to_json()
+    return { "id" => self._id,
+             "user_id" => self.user_id,
+             "email" => self.email,
+             "first_name" => self.first_name,
+             "last_name" => self.last_name,
+             "role" => self.role,
+             "languages" => self.languages,
+             "snooze" => self.snooze,
+             "auth_token" => auth_token,
+             "expiry_time" => expiry_time,
+             "facebook_user" => is_external_user
+    }.to_json
+  end
 
-                                  def to_s
-                                    "#{self.first_name}"
-                                  end
+  def to_s
+    "#{self.first_name}"
+  end
 
-                                  private
-                                  def generate_auth_token()
-                                    self.auth_token = SecureRandom.urlsafe_base64(64, false)
-                                  end
+  private
+  def generate_auth_token()
+    self.auth_token = SecureRandom.urlsafe_base64(64, false)
+  end
 
 
-                                  def calculate_expiry_time()
-                                    now = Time.now
-                                    self.expiry_time = Time.new(now.year, now.month, now.day, 0, 0, 0) + 30.days.to_i
-                                  end
+  def calculate_expiry_time()
+    now = Time.now
+    self.expiry_time = Time.new(now.year, now.month, now.day, 0, 0, 0) + 30.days.to_i
+  end
 
-                                  def user_saved
-                                    EventBus.announce(:user_saved, user_id: _id)
-                                  end
+  def user_saved
+    EventBus.announce(:user_saved, user_id: _id)
+  end
 
-                                  def convert_times_to_utc
-                                    wake_up_time = Time.parse(wake_up)
-                                    go_to_sleep_time = Time.parse(go_to_sleep)
-                                    self.wake_up_in_seconds_since_midnight = User.time_to_seconds_since_midnight wake_up_time, utc_offset
-                                    self.go_to_sleep_in_seconds_since_midnight = User.time_to_seconds_since_midnight go_to_sleep_time, utc_offset
-                                  end
+  def convert_times_to_utc
+    wake_up_time = Time.parse(wake_up)
+    go_to_sleep_time = Time.parse(go_to_sleep)
+    self.wake_up_in_seconds_since_midnight = User.time_to_seconds_since_midnight wake_up_time, utc_offset
+    self.go_to_sleep_in_seconds_since_midnight = User.time_to_seconds_since_midnight go_to_sleep_time, utc_offset
+  end
 
-                                  def self.time_to_seconds_since_midnight time, utc_offset
-                                    #the utc offset should actually be subtracted - look at a map if in doubt
-                                    hour_in_utc = time.hour - utc_offset
-                                    hour_in_utc * 3600 + time.min * 60
-                                  end
+  def self.time_to_seconds_since_midnight time, utc_offset
+    #the utc offset should actually be subtracted - look at a map if in doubt
+    hour_in_utc = time.hour - utc_offset
+    hour_in_utc * 3600 + time.min * 60
+  end
 
-                                  def self.authenticate_password(user, password)
-                                    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-                                      return user
-                                    end
+  def self.authenticate_password(user, password)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      return user
+    end
 
-                                    return nil
-                                  end
+    return nil
+  end
 
-                                  def encrypt_password
-                                    if @password.present?
-                                      self.password_salt = BCrypt::Engine.generate_salt
-                                      self.password_hash = BCrypt::Engine.hash_secret(@password, password_salt)
-                                    end
-                                  end
+  def encrypt_password
+    if @password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(@password, password_salt)
+    end
+  end
 
-                                  def generate_unique_id
-                                    rand = ("817173" + rand(9999999).to_s.center(8, rand(9).to_s)).to_i
-                                    if User.where(:user_id => rand).count > 0
-                                      generate_unique_id
-                                    else
-                                      return rand
-                                    end
-                                  end
+  def generate_unique_id
+    rand = ("817173" + rand(9999999).to_s.center(8, rand(9).to_s)).to_i
+    if User.where(:user_id => rand).count > 0
+      generate_unique_id
+    else
+      return rand
+    end
+  end
 
-                                  def set_unique_id
-                                    unless self.user_id
-                                      unique_id =  generate_unique_id
-                                      self.user_id = unique_id
-                                    end
-                                  end
-                                  end
+  def set_unique_id
+    unless self.user_id
+      unique_id =  generate_unique_id
+      self.user_id = unique_id
+    end
+  end
+end
