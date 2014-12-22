da4791a754fb5e15bf5d9aaa40c81e34cb111774
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-12-22 11:35:15 +0100

    use rescuable correctly

--- event_handlers/assign_last_help_request.rb
+++ event_handlers/assign_last_help_request.rb
@@ -1,8 +1,15 @@
 require_relative './event_handler_base'
 class AssignLastHelpRequest < EventHandlerBase
   def helper_notified(payload)
-    @payload = payload
-    helper.last_help_request = Time.now
-    helper.save!
+    begin
+      @payload = payload
+      if helper.nil?
+        TheLogger.log.error "helper nil in AssignLastHelpRequest"
+      end
+      helper.last_help_request = Time.now
+      helper.save!
+    rescue Exception => e
+      rescue_with_handler(e)
+    end
   end
 end
