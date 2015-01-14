271608508d340badbd99121c203fee975d9a38a5
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-14 16:23:41 +0100

    making language count better

--- mongo_queries/adhoc/languages.js
+++ mongo_queries/adhoc/languages.js
@@ -1,7 +1,9 @@
 load('/usr/local/src/lodash/lodash.min.js');
 
-var languages = []
+var languages = [];
+var language_count = [];
 
+// find unique languages
 db.users.find({},{_id:0,languages:1}).forEach(function(user) {
 languages = _.union(languages, user.languages);
 print(user.languages);
@@ -9,7 +11,14 @@ print(user.languages);
 
 
 _.forEach(languages, function(language) {
-  var helpers_count = db.users.count( { languages: { $in: [ language ] }, role:"helper" } )
+  var helpers_count = db.users.count( { languages: { $in: [ language ] }, role:"helper" } );
+  var blind_count = db.users.count( { languages: { $in: [ language ] }, role:"blind" } );
   print(language);
   print(helpers_count);
+ cnt = { helper_count:helpers_count, language:language, blind_count:blind_count};
+  language_count.push(cnt);
 });
+
+language_count
+_.sortBy(language_count, function(count) { return count.helper_count });
+
