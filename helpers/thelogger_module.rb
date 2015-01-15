a9b60d49fcfe7a102c994a903a7f0bcd7871bf55
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-15 09:49:54 +0100

    not using logstash anymore

--- helpers/thelogger_module.rb
+++ helpers/thelogger_module.rb
@@ -1,6 +1,4 @@
-require 'logstash-logger'
 require_relative 'ambient_request'
-
 
 class BMELogger
  attr_accessor :level
@@ -8,10 +6,6 @@ class BMELogger
 
   def loggger
     @log ||= Logger.new('log/app.log', 'daily')
-  end
-
-  def logstash_logger
-    @logstash_logger ||= LogStashLogger.new(type: :udp, host: 'localhost', port: 3334)
   end
 
   def url
@@ -31,27 +25,22 @@ class BMELogger
   end
 
   def error(message, backtrace = nil)
-    logstash_logger.error message:message, backtrace: backtrace, base_url:  base_url, url: url
     loggger.error message
   end
 
   def debug(message)
-    logstash_logger.debug message:message, base_url:  base_url
     loggger.debug message
   end
 
   def info(message)
-    logstash_logger.info message:message, base_url:  base_url
     loggger.info message
   end
 
   def warn(message)
-    logstash_logger.warn message:message, base_url:  base_url
     loggger.warn message
   end
 
   def fatal(message)
-    logstash_logger.fatal message:message, base_url:  base_url
     loggger.fatal message
   end
 end
