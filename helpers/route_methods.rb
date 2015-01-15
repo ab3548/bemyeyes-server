efdd1b11405bb09f21f110e33757adf65b1a1ed0
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-15 12:08:01 +0100

    handle empty body

--- helpers/route_methods.rb
+++ helpers/route_methods.rb
@@ -2,9 +2,14 @@ class App < Sinatra::Base
   before do
     next unless request.post? || request.put?
     begin
-      body_as_string = request.body.read
-      @body_params = JSON.parse(body_as_string)
-
+      unless request.body.nil?
+        body_as_string = request.body.read
+        if body_as_string.length != 0
+          request.body.rewind
+           @body_params = JSON.parse(body_as_string)
+        end
+        
+      end
     rescue  => e
       TheLogger.log.error "Could not parse body as JSON #{e.message}"
     end
@@ -42,6 +47,7 @@ class App < Sinatra::Base
   end
 
   def model_from_id(id, model_class, code, message)
+    TheLogger.log.fatal "in model_from_id"
     model = model_class.first(:_id => id)
     if model.nil?
       give_error(400, code, message).to_json
