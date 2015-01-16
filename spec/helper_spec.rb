9a23eb4fe25452c5f5ba624fdaa685a617c58eed
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-16 19:52:22 +0100

    since I removed the method, I can't test it

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -56,35 +56,5 @@ describe "Helper" do
       helper.languages = ['ab', 'aa']
       helper.save!
     end
-
-    it "finds no helpers when no speaks blind persons languages" do
-      request = build(:request)
-
-      helper = request.helper
-      helper.languages = ['ab', 'aa']
-      helper.first_name = "non-english"
-      helper.save!
-
-      blind =request.blind
-      blind.languages = ['en']
-      blind.save!
-
-      expect(Helper.helpers_who_speaks_blind_persons_language(request).count).to eq(0)
-    end
-
-    it "finds a helpers when one language overlaps" do
-      request = build(:request)
-
-      helper = request.helper
-      helper.languages = ['ab', 'en']
-      helper.first_name = "non-english"
-      helper.save!
-
-      blind =request.blind
-      blind.languages = ['en', 'da']
-      blind.save!
-
-      expect(Helper.helpers_who_speaks_blind_persons_language(request).count).to eq(1)
-    end
   end
 end
