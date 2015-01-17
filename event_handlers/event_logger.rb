7004fff366436845a84a4072b5737d55578c5cb9
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-17 07:42:08 +0100

    these gets run all the time, so move to fibers

--- event_handlers/event_logger.rb
+++ event_handlers/event_logger.rb
@@ -3,19 +3,25 @@ require_relative '../models/event_log_object'
 class EventLogger
 
   def method_missing(meth, *args, &_block)
-    event_name = meth.to_s
-    event_log = EventLog.new
-    event_log.name = event_name
+    fiber = Fiber.new do
 
-    args[0].each_with_index do |(key, value), index|
-      #first entry is the event_name
-      next if index == 0
-      event_logger_object = EventLogObject.new
-      event_logger_object.name = key
-      event_logger_object.json_serialized = value.to_json
-      event_log.event_log_objects << event_logger_object
+      event_name = meth.to_s
+      event_log = EventLog.new
+      event_log.name = event_name
+
+      args[0].each_with_index do |(key, value), index|
+        #first entry is the event_name
+        next if index == 0
+        event_logger_object = EventLogObject.new
+        event_logger_object.name = key
+        event_logger_object.json_serialized = value.to_json
+        event_log.event_log_objects << event_logger_object
+      end
+      event_log.save!
     end
-    event_log.save!
+
+    fiber.resume
+
   end
 
   def respond_to?(_meth, _include_private = false)
